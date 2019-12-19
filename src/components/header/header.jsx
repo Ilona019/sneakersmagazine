b7242b52e999a1373661b9703b7582e6b7a2f9c6
Ilona019 <ilona.arefieva91119@yandex.ru>
2019-12-20 01:11:21 +0300

    attached forms icons and edited forms styles, add component tooltip.

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,36 +1,30 @@
-import React from 'react';
+import React from "react";
 import LabelProfile from "./../labelProfile/labelProfile";
-import Logo from './../logo/logo';
-import Search from './../search/search';
-import './header.css';
+import Logo from "./../logo/logo";
+import Search from "./../search/search";
+import "./header.css";
 
-import heart from './../../images/heart.svg';
-import ImgHeart from './../imgHeart/imgHeart';
-import shoppingCart from './../../images/shopping-cart.svg';
-import ImgShoppingCart from './../imgShoppingCart/imgShoppingCart';
-import ButtonCount from './../button-count/button-count';
-
-// import PopUpWindows from '../modal/popUpWindows';
-// import LoginForm from '../forms/loginForm';
-// import RegistrationForm from '../forms/registrationForm';
+import heart from "./../../images/heart.svg";
+import ImgHeart from "./../imgHeart/imgHeart";
+import shoppingCart from "./../../images/shopping-cart.svg";
+import ImgShoppingCart from "./../imgShoppingCart/imgShoppingCart";
+import ButtonCount from "./../button-count/button-count";
 
 function Header() {
   return (
-      <div className = "header-class" >
-        <Logo />
-        <Search />
-        <div className = "image-heart">
-          <ImgHeart way = {heart}/>
-          <ButtonCount count = "0" />
-        </div>
-       <div className = "shop-cart">
-        <ImgShoppingCart way = {shoppingCart}/>
-        <ButtonCount count = "0" />
-       </div>
-        {/* <PopUpWindows content={<LoginForm/>}  title="Войти" classHeightContainer="shortContainer" nameIcon="fas fa-sign-in-alt"/>
-        <PopUpWindows content={<RegistrationForm />} title="Регистрация" classHeightContainer="longContainer" nameIcon="fas fa-user-plus"/> */}
-        <LabelProfile />
+    <div className="header-class">
+      <Logo />
+      <Search />
+      <div className="image-heart">
+        <ImgHeart way={heart} />
+        <ButtonCount count="0" />
       </div>
+      <div className="shop-cart">
+        <ImgShoppingCart way={shoppingCart} />
+        <ButtonCount count="0" />
+      </div>
+      <LabelProfile />
+    </div>
   );
 }
 
--- src/components/labelProfile/labelProfile.jsx
+++ src/components/labelProfile/labelProfile.jsx
@@ -1,12 +1,43 @@
-import React from 'react';
-import './labelProfile.css';
+import React from "react";
+import "./labelProfile.css";
+import PopUpWindows from "../modal/popUpWindows";
+import LoginForm from "../forms/loginForm";
+import RegistrationForm from "../forms/registrationForm";
+import Tooltip from "../tooltip/tooltip";
 
-function LabelProfile() {
-  return (
-    <div className="labelProfile-class">
+class LabelProfile extends React.Component {
+  render() {
+    let blockIcons;
+    blockIcons = (
+      <>
+        <Tooltip position="right" content="Войти">
+          <PopUpWindows
+            content={<LoginForm />}
+            title="Войти"
+            classHeightContainer="shortContainer"
+            nameIcon="fas fa-sign-in-alt"
+          />
+        </Tooltip>
+        <Tooltip position="bottom" content="Зарегистрироваться">
+          <PopUpWindows
+            content={<RegistrationForm />}
+            title="Регистрация"
+            classHeightContainer="longContainer"
+            nameIcon="fas fa-user-plus"
+          />
+        </Tooltip>
+      </>
+    );
+
+    return (
+      <div className="labelProfile-class">
         Профиль
-    </div>
-  );
+        <div className="icons-block">
+          <div className="iconsAuth-container">{blockIcons}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default LabelProfile;
